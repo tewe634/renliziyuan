6b7afad55c953bddb015955a13ae1758423e3d4b
werqeqwe <2533314379@qq.com>
2022-08-19 09:45:29 +0800

    完成前置路由守卫

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div :class="{'has-logo':showLogo}">
+    <!-- logo -->
     <logo v-if="showLogo" :collapse="isCollapse" />
     <el-scrollbar wrap-class="scrollbar-wrapper">
       <el-menu
